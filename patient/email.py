f7c78da877086023a6c2da8e7c5ee21e2fc91e0b
Steven Attwell <stevenattwell@hotmail.com>
2021-03-17 17:13:57 +0000

    new email

--- patient/email.py
+++ patient/email.py
@@ -9,44 +9,21 @@ class Email(object):
         self.send()
 
     def send(self):
-        # Replace sender@example.com with your "From" address.
-        # This address must be verified with Amazon SES.
-        sender = "framingham@example.com"
-
-        # Replace recipient@example.com with a "To" address. If your account
-        # is still in the sandbox, this address must be verified.
-        recipient = self._recipient
-
-        # Specify a configuration set. If you do not want to use a configuration
-        # set, comment the following variable, and the
-        # ConfigurationSetName=CONFIGURATION_SET argument below.
-        configuration_set = "ConfigSet"
-
-        # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
-        aws_region = "eu-west-2"
-
-        # The subject line for the email.
-        subject = "Your Framingham Score"
-
-        # The email body for recipients with non-HTML email clients.
-        body_text = ("Framingham results"
-                     )
-
-        # The HTML body of the email.
-        body_html = """<html>
+        # The character encoding for the email.
+        CHARSET = "UTF-8"
+        # Create a new SES resource and specify a region.
+        client = boto3.client('ses', region_name='eu-west-2')
+        SUBJECT = "SUBJECT"
+        BODY_TEXT = "BODY_TEXT"
+        BODY_HTML = """<html>
         <head></head>
         <body>
-          <h1>Framingham Score</h1>
-          <p>Your framingham score for cardiovascular health is {}</p>
+          <h1>{subject}</h1>
+          <p>{message}<br />
+          </p>
         </body>
         </html>
-                    """.format(self._score)
-
-        # The character encoding for the email.
-        charset = "UTF-8"
-
-        # Create a new SES resource and specify a region.
-        client = boto3.client('ses', region_name=aws_region)
+                    """.format(subject=SUBJECT, message=BODY_TEXT)
 
         # Try to send the email.
         try:
@@ -54,30 +31,28 @@ class Email(object):
             response = client.send_email(
                 Destination={
                     'ToAddresses': [
-                        recipient,
+                        self._recipient,
                     ],
                 },
                 Message={
                     'Body': {
                         'Html': {
-                            'Charset': charset,
-                            'Data': body_html,
-                        },
-                        'Text': {
-                            'Charset': charset,
-                            'Data': body_text,
+                            'Charset': CHARSET,
+                            'Data': BODY_HTML,
                         },
                     },
                     'Subject': {
-                        'Charset': charset,
-                        'Data': subject,
+                        'Charset': CHARSET,
+                        'Data': SUBJECT,
                     },
                 },
-                Source=sender,
+                Source="stevenattwell@hotmail.com",
                 # If you are not using a configuration set, comment or delete the
                 # following line
-                ConfigurationSetName=configuration_set,
+                # ConfigurationSetName=CONFIGURATION_SET,
             )
         # Display an error if something goes wrong.
         except ClientError as e:
             print(e.response['Error']['Message'])
+        finally:
+            print("Email sent! Message ID:"),
